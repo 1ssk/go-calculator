51f495de072fa3bc887babb3602fe1aa42f15284
ssk <163558230+1ssk@users.noreply.github.com>
2024-09-27 02:58:51 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+)
+
+// создаём действия
+func add(a, b float64) float64 {
+	return a + b
+}
+
+func subtract(a, b float64) float64 {
+	return a - b
+}
+
+func multiply(a, b float64) float64 {
+	return a * b
+}
+
+func divide(a, b float64) float64 {
+	if b == 0 {
+		fmt.Println("Не делится")
+		return 0
+	}
+	return a / b
+}
+
+// Вводим информацию
+func getInput() (float64, float64, string) {
+	var a, b float64
+	var operator string
+
+	fmt.Print("Введите первое число: ")
+	fmt.Scanln(&a)
+
+	fmt.Print("Введите второе число: ")
+	fmt.Scanln(&b)
+
+	fmt.Print("Введите оператор (+, -, *, /): ")
+	fmt.Scanln(&operator)
+
+	return a, b, operator
+}
+
+// Вычисляем
+func main() {
+	a, b, operator := getInput()
+
+	var result float64
+
+	switch operator {
+	case "+":
+		result = add(a, b)
+	case "-":
+		result = subtract(a, b)
+	case "*":
+		result = multiply(a, b)
+	case "/":
+		result = divide(a, b)
+	default:
+		fmt.Println("Неизвестный оператор")
+		return
+	}
+
+	fmt.Printf("Результат: %.2f\n", result) //Вывод ответа
+}
